4462ed70e65fbc940471c35e23db0476fe42bcef
hknsrr <bilgi@hakansarier.com>
2018-04-21 18:33:46 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" class="logo">
+    <TodoList/>
+  </div>
+</template>
+
+<script>
+import TodoList from "./components/TodoList";
+
+export default {
+  name: "App",
+  components: {
+    TodoList
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./css/variables";
+@mixin box-sizing {
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+}
+
+* {
+  @include box-sizing;
+}
+input,
+textarea,
+select {
+  font-family: inherit;
+  font-size: inherit;
+}
+#app {
+  font-family: $font-stack;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: $primary-color;
+  margin-top: 60px;
+  max-width: 600px;
+  margin: 0 auto;
+
+  .logo {
+    display: block;
+    margin: 0 auto;
+  }
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="todo-app">
+    <h2>{{ msg }} <span v-if="remainingTodos > 0">Yapılacak {{remainingTodos}} iş kaldı.</span></h2>
+    <input type="text" class="todo-input" placeholder="Yapılacak bir şeyler yaz..." v-model="newTodo" @keyup.enter="addTodo" ref="newTodoRef" v-focus>
+    <div v-for="(todo, index) in todos" :key="index" class="todo-item">
+      <input type="checkbox" class="todo-checkbox" v-model="todo.completed" v-on:change="checkTodo">
+      <span v-bind:class="{ completed: todo.completed }">{{ todo.title }}</span>
+      <span class="remove-item" v-on:click="removeTodo(index)">&times;</span>
+    </div>
+    <div v-if="todos.length > 0" class="todo-footer">
+      <input type="checkbox" id="todo-checkbox-all" v-model="checkedAll" class="todo-checkbox" v-on:change="checkAllTodos">
+      <label for="todo-checkbox-all">Hepsini Seç</label>
+      <input type="button" v-if="selectedTodos > 0" class="clear-completed" value="Temizle" v-on:click="clearCompleted">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  data() {
+    return {
+      msg: "Merhaba!",
+      newTodo: "",
+      todos: [
+        {
+          id: 1,
+          title: "Ekmek almaya git.",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Bulaşıkları yıka.",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Akrabalarını ara.",
+          completed: false
+        }
+      ],
+      checkedAll: false
+    };
+  },
+  directives: {
+    focus: {
+      // directive definition
+      inserted: function(el) {
+        el.focus();
+      }
+    }
+  },
+  computed: {
+    remainingTodos() {
+      return this.todos.filter(todo => !todo.completed).length;
+    },
+    selectedTodos() {
+      return this.todos.filter(todo => todo.completed).length;
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.length > 0) {
+        this.todos.push({
+          id: this.todos.length == 0 ? 1 : this.todos.slice(-1)[0].id + 1,
+          title: this.newTodo,
+          completed: false
+        });
+        this.newTodo = "";
+      }
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+      this.$refs.newTodoRef.focus();
+    },
+    clearCompleted() {
+      this.todos = this.todos.filter(todo => !todo.completed);
+      this.checkedAll = false;
+      this.$refs.newTodoRef.focus();
+    },
+    checkAllTodos() {
+      this.todos.forEach(todo => {
+        todo.completed = event.target.checked;
+      });
+    },
+    checkTodo() {
+      this.todos.some(todo => {
+        if (!todo.completed) {
+          this.checkedAll = false;
+          return true;
+        }
+        this.checkedAll = true;
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./../css/variables";
+.todo-app {
+  font-size: $primary-font-size;
+  .todo-input {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 25px;
+  }
+  .todo-item {
+    margin-bottom: 25px;
+    border-bottom: 1px solid #c8c8c8;
+    line-height: 35px;
+    &:hover {
+      .remove-item {
+        display: block;
+      }
+    }
+    .remove-item {
+      float: right;
+      font-weight: bold;
+      font-size: 28px;
+      cursor: pointer;
+      display: none;
+    }
+  }
+  .todo-checkbox {
+    width: 18px;
+    height: 18px;
+    cursor: pointer;
+  }
+  label {
+    font-size: $secondary-font-size;
+  }
+  .completed {
+    text-decoration: line-through;
+    color: gray;
+  }
+  .clear-completed {
+    float: right;
+    border: none;
+    outline: none;
+    background: none;
+    color: $secondary-color;
+    padding: 10px 15px;
+    text-align: center;
+    text-decoration: none;
+    font-size: $secondary-font-size;
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+  label[for="todo-checkbox-all"] {
+    position: relative;
+    bottom: 3px;
+  }
+}
+</style>
